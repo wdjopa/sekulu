67199f10cd19765e26f25410bde7fc2ed6018751
Wilfried <wilfried.djopa@gmail.com>
2019-01-03 14:31:57 +0100

    accueil

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sekulu </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Entete -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark green">
+            <a class="navbar-brand" href="#">Sekulu</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <img src="images/SVG/close-envelope.svg" class="avatar-2" alt="">
+                        <span class="notif">1</span>
+                    </li>
+                    <li class="nav-item">
+                        <img src="images/SVG/notification.svg" class="avatar-2" alt="">
+                        <span class="notif">1</span>
+                    </li>
+                    <li class="nav-item">
+                        <img src="images/SVG/user.svg" class="avatar" alt="">
+                        <a class="text-white">Franck-Olivier NGOUNOU</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Fin entete -->
+
+
+    <!-- Actualites -->
+    <section class="actualites">
+        <div class="grand-bloc">
+            <div class="titre">
+                <h5>Actualités</h5>
+            </div>
+            <div class="bloc">
+                <div class="left-bloc">
+                    <div class="bloc-2 annonce">
+                        <div class="titre">
+                            <h6>Annonce</h6>
+                        </div>
+                        <div class="texte">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugit. Quae fugiat maxime illum ea dicta autem aspernatur saepe ducimus tempora recusandae qui sed, sint harum facilis culpa! Ut, fuga est excepturi saepe harum totam architecto delectus modi tenetur explicabo. Eius quod adipisci accusamus placeat omn ...
+                        </div>
+                        <div class="btn d-flex justify-content-end">
+                            <a href="" class="btn btn-large btn-outline-danger">Voir plus</a>
+                        </div>
+                    </div>
+                    <div class="bloc-2 nouveautes">
+                        <div class="titre">
+                            <h6>Des nouveautés à l'école</h6>
+                        </div>
+                        <div class="texte">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugit. Quae fugiat maxime illum ea dicta autem aspernatur saepe ducimus tempora recusandae qui sed, sint harum facilis culpa! Ut, fuga est excepturi saepe harum totam architecto delectus modi tenetur explicabo. Eius quod adipisci accusamus placeat omn ...
+                        </div>
+                        <div class="btn d-flex justify-content-end">
+                            <a href="" class="btn btn-large btn-outline-danger">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-bloc">
+                    <!-- SlideShow -->
+                </div>
+            </div>
+        </div>  
+    </section>
+    <!-- Fin Actualites -->
+
+    <!-- Mes Notes -->
+    <section class="notes">
+        <div class="grand-bloc">
+            <div class="titre">
+                <h5>Mes notes</h5>
+            </div>
+            <div class="bloc">
+                <div class="note-bloc">
+                    
+                <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+
+                    <div class="note">
+                        <div class="note-val">
+                            <div class="valeur">
+                                <span class="entiere">16</span>
+                                <sup class="decimale">75</sup>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="matiere">Algèbre</span>
+                            <span class="enseignant">Dr. CIAKE CIAKE F.</span>
+                            <span class="date">Rendu le 07/12/2018</span>
+                        </div>
+                    </div>
+                </div>
+                <a href="" class="text-danger">Voir toutes les notes</a>
+            </div>
+        </div>  
+    </section>
+    <!-- Fin Mes Notes -->
+
+</body>
+</html>
